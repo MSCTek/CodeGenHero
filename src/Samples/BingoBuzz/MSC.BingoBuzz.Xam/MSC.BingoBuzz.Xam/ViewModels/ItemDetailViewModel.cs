1217e4f0ababd982cf08b81e639d1f87ee0e06ec
robin@msctek.com <robin@msctek.com>
2018-02-10 13:52:35 -0600

    Populated Xam project with MasterDetails app, used CGH to generate models and mappers.

---
+++ src/Samples/BingoBuzz/MSC.BingoBuzz.Xam/MSC.BingoBuzz.Xam/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MSC.BingoBuzz.Xam.Models;
+
+namespace MSC.BingoBuzz.Xam.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
