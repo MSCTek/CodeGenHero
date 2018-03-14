98726059611701c08d69abf745c6bcc5cb9db4bf
Paul Schroeder <paul@msctek.com>
2018-03-13 20:06:32 -0500

    Fix for AutoMapper initializer.

---
+++ src/Samples/BingoBuzz/CodeGenHero.BingoBuzz.Repository/Mappers/AutoMapperInitializer.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace CodeGenHero.BingoBuzz.Repository.Mappers
+{
+	public class AutoMapperInitializer
+	{
+		public static void Initialize()
+		{
+			Mapper.Initialize(cfg =>
+			{
+				cfg.AddProfiles(typeof(AutoMapperInitializer));
+			});
+		}
+	}
+}
