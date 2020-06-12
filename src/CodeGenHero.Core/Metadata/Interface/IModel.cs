d2e37ab003b9f0e06c07023628ee650c6c9c7408
Paul Schroeder <paul@msctek.com>
2020-06-11 23:26:11 -0500

    Added IModel convenience methods to assist in filtering by regular expressions: GetEntityTypesByRegEx, GetEntityTypesFilteredByRegEx,  and GetEntityTypesMatchingRegEx.

--- src/CodeGenHero.Core/Metadata/Interface/IModel.cs
+++ src/CodeGenHero.Core/Metadata/Interface/IModel.cs
@@ -7,42 +7,48 @@ using System.Text;
 
 namespace CodeGenHero.Core.Metadata
 {
-	public interface IModel
-	{
-		/// <summary>
-		///     Gets all entity types defined in the model.
-		/// </summary>
-		/// <returns> All entity types defined in the model. </returns>
-		IList<IEntityType> EntityTypes { get; }
+    public interface IModel
+    {
+        /// <summary>
+        ///     Gets all entity types defined in the model.
+        /// </summary>
+        /// <returns> All entity types defined in the model. </returns>
+        IList<IEntityType> EntityTypes { get; }
 
-		string ModelFullyQualifiedTypeName { get; set; }
-		string ModelTypeName { get; set; }
+        string ModelFullyQualifiedTypeName { get; set; }
+        string ModelTypeName { get; set; }
 
-		/// <summary>
-		///     Gets the entity type with the given name. Returns null if no entity type with the given name is found
-		///     or the entity type has a defining navigation.
-		/// </summary>
-		/// <param name="name"> The name of the entity type to find. </param>
-		/// <returns> The entity type, or null if none are found. </returns>
-		IEntityType FindEntityType([NotNull] string name);
+        /// <summary>
+        ///     Gets the entity type with the given name. Returns null if no entity type with the given name is found
+        ///     or the entity type has a defining navigation.
+        /// </summary>
+        /// <param name="name"> The name of the entity type to find. </param>
+        /// <returns> The entity type, or null if none are found. </returns>
+        IEntityType FindEntityType([NotNull] string name);
 
-		/// <summary>
-		///     Gets the entity type for the given name, defining navigation name
-		///     and the defining entity type. Returns null if no matching entity type is found.
-		/// </summary>
-		/// <param name="name"> The name of the entity type to find. </param>
-		/// <param name="definingNavigationName"> The defining navigation of the entity type to find. </param>
-		/// <param name="definingEntityType"> The defining entity type of the entity type to find. </param>
-		/// <returns> The entity type, or null if none are found. </returns>
-		IEntityType FindEntityType(
-			[NotNull] string name,
-			[NotNull] string definingNavigationName,
-			[NotNull] IEntityType definingEntityType);
+        /// <summary>
+        ///     Gets the entity type for the given name, defining navigation name
+        ///     and the defining entity type. Returns null if no matching entity type is found.
+        /// </summary>
+        /// <param name="name"> The name of the entity type to find. </param>
+        /// <param name="definingNavigationName"> The defining navigation of the entity type to find. </param>
+        /// <param name="definingEntityType"> The defining entity type of the entity type to find. </param>
+        /// <returns> The entity type, or null if none are found. </returns>
+        IEntityType FindEntityType(
+            [NotNull] string name,
+            [NotNull] string definingNavigationName,
+            [NotNull] IEntityType definingEntityType);
 
-		/// <summary>
-		///     Gets all entity types defined in the model.
-		/// </summary>
-		/// <returns> All entity types defined in the model. </returns>
-		IList<IEntityType> GetEntityTypes();
-	}
+        /// <summary>
+        ///     Gets all entity types defined in the model.
+        /// </summary>
+        /// <returns> All entity types defined in the model. </returns>
+        IList<IEntityType> GetEntityTypes();
+
+        IList<IEntityType> GetEntityTypesByRegEx(string excludeRegExPattern, string includeRegExPattern);
+
+        IList<IEntityType> GetEntityTypesFilteredByRegEx(string regExPattern);
+
+        IList<IEntityType> GetEntityTypesMatchingRegEx(string regExPattern);
+    }
 }
--- src/CodeGenHero.Core/Metadata/Model.cs
+++ src/CodeGenHero.Core/Metadata/Model.cs
@@ -4,31 +4,88 @@ using JetBrains.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace CodeGenHero.Core.Metadata
 {
-	[Serializable]
-	public class Model : MetadataBase, IModel
-	{
-		public IList<IEntityType> EntityTypes { get; set; } = new List<IEntityType>();
+    [Serializable]
+    public class Model : MetadataBase, IModel
+    {
+        public IList<IEntityType> EntityTypes { get; set; } = new List<IEntityType>();
 
-		public string ModelFullyQualifiedTypeName { get; set; }
+        public string ModelFullyQualifiedTypeName { get; set; }
 
-		public string ModelTypeName { get; set; }
+        public string ModelTypeName { get; set; }
 
-		public IEntityType FindEntityType([NotNull] string name)
-		{
-			return EntityTypes.FirstOrDefault(x => x.Name == name);
-		}
+        public IEntityType FindEntityType([NotNull] string name)
+        {
+            return EntityTypes.FirstOrDefault(x => x.Name == name);
+        }
 
-		public IEntityType FindEntityType([NotNull] string name, [NotNull] string definingNavigationName, [NotNull] IEntityType definingEntityType)
-		{
-			return EntityTypes.FirstOrDefault(x => x.Name == name && x.DefiningNavigationName == definingNavigationName && x.DefiningEntityType == x.DefiningEntityType);
-		}
+        public IEntityType FindEntityType([NotNull] string name, [NotNull] string definingNavigationName, [NotNull] IEntityType definingEntityType)
+        {
+            return EntityTypes.FirstOrDefault(x => x.Name == name && x.DefiningNavigationName == definingNavigationName && x.DefiningEntityType == x.DefiningEntityType);
+        }
 
-		public IList<IEntityType> GetEntityTypes()
-		{
-			return EntityTypes;
-		}
-	}
+        public IList<IEntityType> GetEntityTypes()
+        {
+            return EntityTypes;
+        }
+
+        public IList<IEntityType> GetEntityTypesByRegEx(string excludeRegExPattern, string includeRegExPattern)
+        {
+            IList<IEntityType> retVal = GetEntityTypesFilteredByRegEx(excludeRegExPattern);
+            retVal.AddRange(GetEntityTypesMatchingRegEx(includeRegExPattern));
+
+            // Make sure we return a distinct list in case something does not get excluded and then is specifically included.
+            retVal = retVal.GroupBy(elem => elem.Name).Select(group => group.First()).ToList();
+            return retVal;
+        }
+
+        public IList<IEntityType> GetEntityTypesFilteredByRegEx(string regExPattern)
+        {
+            List<IEntityType> retVal = new List<IEntityType>();
+
+            if (EntityTypes == null)
+            {
+                return retVal;
+            }
+            else if (string.IsNullOrWhiteSpace(regExPattern))
+            {
+                return EntityTypes;
+            }
+
+            foreach (var entityType in EntityTypes)
+            {
+                var match = Regex.Match(entityType.ClrType.Name, regExPattern, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+                if (!match.Success)
+                {
+                    retVal.Add(entityType);
+                }
+            }
+
+            return retVal;
+        }
+
+        public IList<IEntityType> GetEntityTypesMatchingRegEx(string regExPattern)
+        {
+            List<IEntityType> retVal = new List<IEntityType>();
+
+            if (EntityTypes == null || string.IsNullOrWhiteSpace(regExPattern))
+            {
+                return retVal;
+            }
+
+            foreach (var entityType in EntityTypes)
+            {
+                var match = Regex.Match(entityType.ClrType.Name, regExPattern, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+                if (match.Success)
+                {
+                    retVal.Add(entityType);
+                }
+            }
+
+            return retVal;
+        }
+    }
 }
